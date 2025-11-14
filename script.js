ea52d4154f4f7baa507eef546158f730f796d041
karenCaPa <Karen_camachopa@fet.edu.co>
2025-11-14 15:38:50 -0500

    feat: agregar suma y resta

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("Calculadora iniciada (estructura base)");
+function obtenerNumeros() {
+    const n1 = parseFloat(document.getElementById("numero1").value);
+    const n2 = parseFloat(document.getElementById("numero2").value);
+    return { n1, n2 };
+}
+
+function mostrarResultado(valor) {
+    document.getElementById("resultado").textContent = valor;
+}
+
+function sumar() {
+    const { n1, n2 } = obtenerNumeros();
+    mostrarResultado(n1 + n2);
+}
+
+function restar() {
+    const { n1, n2 } = obtenerNumeros();
+    mostrarResultado(n1 - n2);
+}
